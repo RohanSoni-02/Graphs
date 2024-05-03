0e05f0a2f9eec7f9a5d916935c195cd185b774c1
RohanSoni-02 <soni.rohanchd@gmail.com>
2024-05-03 17:01:11 +0530

    added add vertex function

--- Graphs/Graphs/main.cpp
+++ Graphs/Graphs/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+class Graph{
+private:
+    unordered_map<string, unordered_set<string>> adjList;
+public:
+    bool addVertex(string vertex){
+        if(adjList.count(vertex) == 0){
+            adjList[vertex];
+            return true;
+        }
+        return false;
+    }
+    
+    void printGraph(){
+        for(auto [vertex,edges]: adjList){
+            cout<< vertex << ": [ ";
+            for (auto edge: edges){
+                cout<< edge << " ";
+            }
+            cout<< "]" <<endl;
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Graph* myGraph = new Graph();
+    myGraph->addVertex("A");
+    myGraph->printGraph();
 }

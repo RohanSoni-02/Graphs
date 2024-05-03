59756cfba98472eb9701bec0198ae2dc46de52ca
RohanSoni-02 <soni.rohanchd@gmail.com>
2024-05-03 17:08:37 +0530

    added add edge

--- Graphs/Graphs/main.cpp
+++ Graphs/Graphs/main.cpp
@@ -30,10 +30,22 @@ public:
             cout<< "]" <<endl;
         }
     }
+    
+    bool addEdge(string vertex1, string vertex2){
+        if (adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0) {
+            adjList.at(vertex1).insert(vertex2);
+            adjList.at(vertex2).insert(vertex1);
+            return true;
+        }
+        return false;
+    }
 };
 
 int main(int argc, const char * argv[]) {
     Graph* myGraph = new Graph();
     myGraph->addVertex("A");
+    myGraph->addVertex("B");
+    myGraph->addVertex("C");
+    myGraph->addEdge("A","C");
     myGraph->printGraph();
 }
